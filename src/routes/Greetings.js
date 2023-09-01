836771893cb2dbba1e072b1a463745255526276e
Ouail-01 <ouaillaarabi@gmail.com>
2023-09-01 18:48:08 +0100

    Add routes folder

---
+++ src/routes/Greetings.js
@@ -0,0 +1,11 @@
+/* eslin-disable */
+import React from 'react';
+import MyGreetings from '../components/greetings';
+
+export default function Greetings() {
+  return (
+    <div>
+      <MyGreetings />
+    </div>
+  );
+}
